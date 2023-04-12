0813b163e9bd30c40c378e014f8c7664e025bf3b
Denis Mavrović <denis.mavrovic@gmail.com>
2023-04-12 04:47:44 +0200

    Pull request changes

--- Ispit.Narudzba/Distributer.cs
+++ Ispit.Narudzba/Distributer.cs
@@ -8,15 +8,16 @@ namespace Ispit.Narudzba
 {
     internal class Distributer
     {
-        public delegate void NarudzbaNarucena();
+        public delegate void NarudzbaNarucena(DateTime dateTime);
         public event NarudzbaNarucena NarudzbaUIsporuci;
 
-        public void KrecemSDostavom(DateTime vrijeme)
+        //public void KrecemSDostavom(DateTime vrijeme)
+        public void KrecemSDostavom()
         {
             Console.WriteLine("brmm, brmm... Kamion je krenio. brmm, brmm....");
-            Console.WriteLine("Narudžba je istovarena u kamion i krenuo {0}", vrijeme);
+            //Console.WriteLine("Narudžba je istovarena u kamion i krenuo {0}", vrijeme);
 
-            NarudzbaUIsporuci?.Invoke();
+            NarudzbaUIsporuci?.Invoke(DateTime.Now);
         }
     }
 }
--- Ispit.Narudzba/Program.cs
+++ Ispit.Narudzba/Program.cs
@@ -8,20 +8,38 @@ namespace Ispit.Narudzba
 {
     internal class Program
     {
-        public delegate DateTime NarudzbaNarucena();
+        public delegate void NarudzbaNarucena(DateTime vrijemeNarudzbe);
         static void Main(string[] args)
         {
             Narudzba narudzba1 = new Narudzba();
             Narudzba narudzba2 = new Narudzba();
             Distributer distributer = new Distributer();
 
+            narudzba1.NarudzbaPredana += PretplacenaMetoda;
+            narudzba2.NarudzbaPredana += PretplacenaMetoda;
+            distributer.NarudzbaUIsporuci += Distributer_NarudzbaUIsporuci;
+
             narudzba1.Naziv = "Banana";
-            narudzba2.Naziv = "Jabuka";
-            narudzba1.KreirajNarudzbu();
-            narudzba2.KreirajNarudzbu();
-            distributer.KrecemSDostavom(DateTime.Now);
+            narudzba2.Naziv = "Jabuka";            
+            PretplacenaMetoda(narudzba1);
+            PretplacenaMetoda(narudzba2);
+            //distributer.KrecemSDostavom();
+            Distributer_NarudzbaUIsporuci(DateTime.Now);
+            
 
             Console.ReadKey();
+        }
+
+        private static void Distributer_NarudzbaUIsporuci(DateTime vrijeme)
+        {
+            Console.WriteLine("brmm, brmm... Kamion je krenio. brmm, brmm....");
+            Console.WriteLine("Narudžba je istovarena u kamion i krenuo {0}", vrijeme);
+        }
+
+        private static void PretplacenaMetoda(Narudzba narudzba)
+        {
+            Console.WriteLine("Narudžba {0} naručena.", narudzba.Naziv);
+            Console.WriteLine("Narudžba {0} je predna distributeru.", narudzba.Naziv);
         }
     }
 }
